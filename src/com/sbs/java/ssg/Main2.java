5bfcadae63b3a34789f4799cf06455924721b6a3
dbwnstjs4862 <dbwnstjs4862@gmail.com>
2023-07-05 09:22:46 +0900

    게시물 작성 기능 구현중

---
+++ src/com/sbs/java/ssg/Main2.java
@@ -0,0 +1,34 @@
+package com.sbs.java.ssg;
+
+
+import java.util.Scanner;
+
+public class Main2 {
+	public static void main(String[] args) {
+		System.out.println("== 프로그램 시작 ==");
+		Scanner sc = new Scanner(System.in);
+		
+		while ( true ) {
+			System.out.printf("명령어) ");
+			String command = sc.nextLine();
+			command = command.trim();
+			
+			if ( command.length() == 0 ) {
+				continue;
+			}
+			
+			if ( command.equals("system exit") ) {
+				break;
+			}
+			else if ( command.equals("article list") ) {
+				System.out.println("게시물이 없습니다.");
+			}
+			else {
+				System.out.printf("%s(은)는 존재하지 않는 명령어 입니다.\n", command);
+			}
+		}
+		
+		sc.close();	
+		System.out.println("== 프로그램 끝 ==");
+	}
+}
