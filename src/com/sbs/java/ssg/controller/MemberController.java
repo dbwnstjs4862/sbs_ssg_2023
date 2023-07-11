a3977284d0dd2cf38347156650d2b1a5bbb5979b
dbwnstjs4862 <dbwnstjs4862@gmail.com>
2023-07-11 09:18:08 +0900

    액션이 실행되기 전에 가드에 의해서 말도 안되는 요청들 필터링

--- src/com/sbs/java/ssg/controller/MemberController.java
+++ src/com/sbs/java/ssg/controller/MemberController.java
@@ -126,11 +126,6 @@ public class MemberController extends Controller {
 	}
 
 	private void doLogin() {
-		if (isLogined()) {
-			System.out.println("이미 로그인이 되어 있습니다.");
-			return;
-		}
-
 		System.out.printf("로그인 아이디 : ");
 		String loginId = sc.nextLine();
 		System.out.printf("로그인 비번 : ");
@@ -155,11 +150,6 @@ public class MemberController extends Controller {
 	}
 
 	private void doLogout() {
-		if (isLogined() == false) {
-			System.out.println("로그인 상태가 아닙니다.");
-			return;
-		}
-
 		loginedMember = null;
 
 		System.out.println("로그아웃 되었습니다.");
