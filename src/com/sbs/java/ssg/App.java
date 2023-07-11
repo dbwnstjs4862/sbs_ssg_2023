1eef20adc8eb58791fb938c7c548b11105204e82
dbwnstjs4862 <dbwnstjs4862@gmail.com>
2023-07-11 12:10:03 +0900

    articleService 도입

--- src/com/sbs/java/ssg/App.java
+++ src/com/sbs/java/ssg/App.java
@@ -11,10 +11,10 @@ public class App {
 		System.out.println("== 프로그램 시작 ==");
 
 		Scanner sc = new Scanner(System.in);
-
+		
 		MemberController memberController = new MemberController(sc);
 		ArticleController articleController = new ArticleController(sc);
-
+		
 		articleController.makeTestData();
 		memberController.makeTestData();
 
@@ -30,50 +30,51 @@ public class App {
 			if (command.equals("system exit")) {
 				break;
 			}
-
+			
 			String[] commandBits = command.split(" "); // article detail
-
-			if (commandBits.length == 1) {
+			
+			if ( commandBits.length == 1 ) {
 				System.out.println("존재하지 않는 명령어 입니다.");
 				continue;
 			}
-
-			String controllerName = commandBits[0]; // article
+			
+			String controllerName = commandBits[0]; // article 
 			String actionMethodName = commandBits[1]; // detail
-
+			
 			Controller controller = null;
-
-			if (controllerName.equals("article")) {
+			
+			if ( controllerName.equals("article") ) {
 				controller = articleController;
-			} else if (controllerName.equals("member")) {
+			} else if ( controllerName.equals("member") ) {
 				controller = memberController;
 			}
-
+			
 			String actionName = controllerName + "/" + actionMethodName;
 			// article/list
-
+			
 			switch (actionName) {
 			case "article/write":
 			case "article/delete":
 			case "article/modify":
 			case "member/logout":
-				if (Controller.isLogined() == false) {
+				if ( Controller.isLogined() == false ) {
 					System.out.println("로그인 후 이용해주세요.");
 					continue;
 				}
 				break;
 			}
-
+			
 			switch (actionName) {
 			case "member/login":
 			case "member/join":
-				if (Controller.isLogined()) {
+				if ( Controller.isLogined() ) {
 					System.out.println("로그아웃 후 이용해주세요.");
 					continue;
 				}
 				break;
 			}
-
+			
+			
 			controller.doAction(command, actionMethodName);
 		}
 
