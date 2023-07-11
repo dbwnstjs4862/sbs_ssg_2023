4bc08adfcf48fb5cc757880dbf9a208702d56722
dbwnstjs4862 <dbwnstjs4862@gmail.com>
2023-07-11 12:21:05 +0900

    회원 서비스 도입

--- src/com/sbs/java/ssg/controller/MemberController.java
+++ src/com/sbs/java/ssg/controller/MemberController.java
@@ -1,23 +1,21 @@
 package com.sbs.java.ssg.controller;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 import com.sbs.java.ssg.container.Container;
-import com.sbs.java.ssg.dto.Article;
 import com.sbs.java.ssg.dto.Member;
+import com.sbs.java.ssg.service.MemberService;
 import com.sbs.java.ssg.util.Util;
 
 public class MemberController extends Controller {
 	private Scanner sc;
-	private List<Member> members;
 	private String command;
 	private String actionMethodName;
+	private MemberService memberService;
 	
 	public MemberController(Scanner sc) {
 		this.sc = sc;
-		members = Container.memberDao.members;
+		memberService = Container.memberService;
 	}
 	
 	public void doAction(String command, String actionMethodName) {
@@ -43,37 +41,13 @@ public class MemberController extends Controller {
 	public void makeTestData() {
 		System.out.println("테스트를 위한 데이터를 생성합니다.");
 		
-		Container.memberDao.add(new Member(Container.memberDao.getNewId(), Util.getNowDateStr(), "admin", "admin", "관리자"));
-		Container.memberDao.add(new Member(Container.memberDao.getNewId(), Util.getNowDateStr(), "user1", "user1", "홍길동"));
-		Container.memberDao.add(new Member(Container.memberDao.getNewId(), Util.getNowDateStr(), "user2", "user2", "홍길순"));
-	}
-
-	private int getMemberIndexByLoginId(String loginId) {
-		int i = 0;
-		
-		for ( Member member : members ) {
-			if ( member.loginId.equals(loginId) ) {
-				return i;
-			}
-			
-			i++;
-		}
-		
-		return -1;
-	}
-	
-	private Member getMemberByLoginId(String loginId) {
-		int index = getMemberIndexByLoginId(loginId);
-		
-		if ( index == -1 ) {
-			return null;
-		}
-		
-		return members.get(index);
+		memberService.join(new Member(Container.memberDao.getNewId(), Util.getNowDateStr(), "admin", "admin", "관리자"));
+		memberService.join(new Member(Container.memberDao.getNewId(), Util.getNowDateStr(), "user1", "user1", "홍길동"));
+		memberService.join(new Member(Container.memberDao.getNewId(), Util.getNowDateStr(), "user2", "user2", "홍길순"));
 	}
 
 	private boolean isJoinableLoginId(String loginId) {
-		int index = getMemberIndexByLoginId(loginId);
+		int index = memberService.getMemberIndexByLoginId(loginId);
 		
 		if ( index == -1 ) {
 			return true;
@@ -121,7 +95,7 @@ public class MemberController extends Controller {
 		String name = sc.nextLine();
 
 		Member member = new Member(id, regDate, loginId, loginPw, name);
-		Container.memberDao.add(member);
+		memberService.join(member);
 
 		System.out.printf("%d번 회원이 생성되었습니다. 환영합니다^^\n", id);
 	}
@@ -133,7 +107,7 @@ public class MemberController extends Controller {
 		String loginPw = sc.nextLine();
 		
 		// 입력받은 아이디에 해당하는 회원이 존재하는지
-		Member member = getMemberByLoginId(loginId);
+		Member member = memberService.getMemberByLoginId(loginId);
 		
 		if ( member == null ) {
 			System.out.println("해당 회원은 존재하지 않습니다.");
